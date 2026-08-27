18382a1fca61a88cbb11358cd380acda342be073
bro164 <seriybro164@gmail.com>
2026-08-27 19:28:58 +0300

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,2 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/bro164/astralmm2/refs/heads/main/1.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/bro164/astralmm2/refs/heads/main/2.lua"))()
